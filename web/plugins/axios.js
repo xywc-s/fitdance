44627f0eee47d4b67da50b90524ab77f5d3d6095
易长松 <xywc_s@foxmail.com>
2020-12-24 14:29:00 +0800

    debug

--- web/plugins/axios.js
+++ web/plugins/axios.js
@@ -1,8 +1,16 @@
-export default function ({ $axios }) {
+export default function ({ $axios, redirect }) {
   $axios.onResponse((response) => {
     const res = response.data
     if (res.code === 20000) {
       return res.data
     }
+  })
+  $axios.onError(err => {
+    redirect('/index')
+    Promise.reject(error)
+  })
+
+  $axios.onResponseError((error) => {
+    Promise.reject(error)
   })
 }
