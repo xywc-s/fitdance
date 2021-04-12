6374373cd5c54ef756ca7d24137fd1f8e3d0b833
易长松 <xywc_s@foxmail.com>
2021-04-12 11:30:07 +0800

    删除工厂图片背景

--- web/components/Factory.vue
+++ web/components/Factory.vue
@@ -3,7 +3,7 @@
     <title-com :title="title" :subtitle="subtitle" class="py-16"></title-com>
     <v-row no-gutters>
       <v-row class="col-6 d-flex my-2" no-gutters>
-        <v-col class="left col-7">
+        <v-col class="col-7">
           <v-img :src="factories[0]" height="100%">
             <template v-slot:placeholder>
               <v-row class="fill-height ma-0" align="center" justify="center">
@@ -12,7 +12,7 @@
             </template>
           </v-img>
         </v-col>
-        <v-col class="center col-5 d-flex flex-column px-2">
+        <v-col class="col-5 d-flex flex-column px-2">
           <v-img :src="factories[1]" width="100%">
             <template v-slot:placeholder>
               <v-row class="fill-height ma-0" align="center" justify="center">
@@ -36,7 +36,7 @@
           </v-img>
         </v-col>
       </v-row>
-      <v-col class="right col-6 my-2">
+      <v-col class="col-6 my-2">
         <v-img :src="factories[4]" aspect-ratio="1.1">
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
@@ -64,16 +64,4 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-#factory {
-  .left {
-    background-color: greenyellow;
-  }
-  .center {
-    background-color: khaki;
-  }
-  .right {
-    background-color: indianred;
-  }
-}
-</style>
+
