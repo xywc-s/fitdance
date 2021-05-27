a9430a1fcb2e343f14821423b21d2d1d166e46a5
易长松 <xywc_s@foxmail.com>
2021-05-27 15:22:49 +0800

    公司信息修改

--- web/components/CompanyInfo.vue
+++ web/components/CompanyInfo.vue
@@ -4,13 +4,13 @@
     <div :class="[subTitle ?subTitle: 'text-caption text--disabled', 'd-flex','ml-3']">
       <div style="line-height:30px">
         <div>电话：0717-3222718 / {{tel}}</div>
-        <div>邮箱：wdf.88@outlook.com</div>
+        <div>邮箱：info@fitdancetutu.com</div>
         <div>网址：www.fitdance.cn</div>
         <div>地址：湖北省当阳市玉阳办事处南正街70号</div>
       </div>
       <div v-if="showSubContact" class="ml-12" style="line-height:30px">
         <div>微信：{{tel}}</div>
-        <div>QQ：wdf.88@outlook.com</div>
+        <div>QQ：717659371</div>
       </div>
     </div>
   </div>
