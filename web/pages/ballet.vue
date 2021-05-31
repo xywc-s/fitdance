d1337df18fec3d25cbccc2f7281c0452d1e7ceb5
易长松 <xywc_s@foxmail.com>
2021-05-31 14:51:09 +0800

    芭蕾页面修改

--- web/pages/ballet.vue
+++ web/pages/ballet.vue
@@ -3,35 +3,28 @@
     <img class="max-w-100" :src="banner" />
     <div class="container-fuild">
       <FTitle class="mt-12">热门产品</FTitle>
-      <v-row no-gutters class="relative mt-4" style="z-index:1">
-        <v-col>
-          <img src="../static/products/ballet/蓝色芭蕾裙01.png" class="d-block" />
+      <v-row
+        no-gutters
+        class="relative mt-4"
+        style="z-index:1"
+        v-for="(hot,i) in hotProducts"
+        :key="i"
+      >
+        <v-col v-if="!i">
+          <img :src="hot[0]" class="d-block" />
         </v-col>
-        <v-col class="d-flex flex-column justify-end pl-5">
-          <div class="text-left">
-            <div class="font-22 color-3 mb-2">蓝色芭蕾舞裙</div>
+        <v-col :class="['d-flex', 'flex-column', 'justify-end', `p${i?'r':'l'}-5`]">
+          <div :class="`text-${i?'right':'left'}`">
+            <div class="font-22 color-3 mb-2">{{i?'红色':'蓝色'}}芭蕾舞裙</div>
             <div class="font-14 color-6 mb-4">松紧腰身和抽绳设计，背部搭扣，9-11层裙边叠加</div>
           </div>
           <div class="d-flex justify-space-between">
-            <img src="../static/products/ballet/蓝色芭蕾裙02.png" class="d-block" />
-            <img src="../static/products/ballet/蓝色芭蕾裙03.png" class="d-block" />
+            <img :src="hot[1]" class="d-block" />
+            <img :src="hot[2]" class="d-block" />
           </div>
         </v-col>
-        <div class="bg-border"></div>
-      </v-row>
-      <v-row no-gutters class="relative mt-10" style="z-index:1">
-        <v-col class="d-flex flex-column justify-end pr-5">
-          <div class="text-right">
-            <div class="font-22 color-3 mb-2">红色芭蕾舞裙</div>
-            <div class="font-14 color-6 mb-4">松紧腰身和抽绳设计，背部搭扣，9-11层裙边叠加</div>
-          </div>
-          <div class="d-flex justify-space-between">
-            <img src="../static/products/ballet/红色芭蕾裙01.png" class="d-block" />
-            <img src="../static/products/ballet/红色芭蕾裙02.png" class="d-block" />
-          </div>
-        </v-col>
-        <v-col>
-          <img src="../static/products/ballet/红色芭蕾裙03.png" class="d-block" />
+        <v-col v-if="i">
+          <img :src="hot[0]" class="d-block" />
         </v-col>
         <div class="bg-border"></div>
       </v-row>
@@ -48,26 +41,26 @@
       <section class="mt-12">
         <FTitle>展会实拍</FTitle>
         <div class="relative live-shot font-12 color-6 text-left">
-          <img class="max-w-100" src="../static/products/ballet/liveshot/展会底部.png" />
+          <img class="max-w-100" :src="liveShot[0]" />
           <div class="text-r">
             <div>芭蕾舞</div>
             <div>孕育于十四世纪,意大利文艺复兴时期，降生在十七世纪后期路易十四的法国宫廷</div>
           </div>
           <div class="photo d-flex justify-end text-right">
             <div class="mr-11">
-              <img src="../static/products/ballet/liveshot/展会实拍01.png" />
+              <img :src="liveShot[1]" />
               <div class="mt-10 mb-12">
                 <span
                   class="d-inline-block text-justify text-left mr-5"
                   style="width:360px"
                 >著名芭蕾舞剧：《天鹅湖》、《胡桃夹子》、《罗密欧与朱丽叶》、《吉赛尔》、《舞姬》等</span>
               </div>
-              <img class="pb-4" src="../static/products/ballet/liveshot/展会实拍03.png" />
-              <img class="mt-16 pt-8" src="../static/products/ballet/liveshot/展会实拍05.png" />
+              <img class="pb-4" :src="liveShot[3]" />
+              <img class="mt-16 pt-8" :src="liveShot[5]" />
             </div>
             <div class="d-flex flex-column">
-              <img src="../static/products/ballet/liveshot/展会实拍02.png" />
-              <img class="mt-16 pt-16" src="../static/products/ballet/liveshot/展会实拍04.png" />
+              <img :src="liveShot[2]" />
+              <img class="mt-16 pt-16" :src="liveShot[4]" />
             </div>
           </div>
           <div class="text-b">
@@ -84,7 +77,7 @@
 </template>
 
 <script>
-const banner = require('../static/products/ballet/banner.png')
+const banner = require('../static/products/ballet/banner.jpg')
 const products = [
   require('../static/products/ballet/show/产品01.png'),
   require('../static/products/ballet/show/产品02.png'),
@@ -100,11 +93,33 @@ const products = [
   require('../static/products/ballet/show/产品12.png'),
 ]
 
+const hotProducts = [
+  [
+    require('../static/products/ballet/蓝色芭蕾裙01.png'),
+    require('../static/products/ballet/蓝色芭蕾裙02.png'),
+    require('../static/products/ballet/蓝色芭蕾裙03.png')
+  ], [
+    require('../static/products/ballet/红色芭蕾裙03.png'),
+    require('../static/products/ballet/红色芭蕾裙01.png'),
+    require('../static/products/ballet/红色芭蕾裙02.png')
+  ]
+]
+
+const liveShot = [
+  require('../static/products/ballet/liveshot/展会底部.png'),
+  require('../static/products/ballet/liveshot/展会实拍01.png'),
+  require('../static/products/ballet/liveshot/展会实拍02.png'),
+  require('../static/products/ballet/liveshot/展会实拍03.png'),
+  require('../static/products/ballet/liveshot/展会实拍04.png'),
+  require('../static/products/ballet/liveshot/展会实拍05.png')
+]
 export default {
   data() {
     return {
       banner,
-      products
+      products,
+      liveShot,
+      hotProducts
     }
   }
 }
