59365778c79f9d7ecd743add5fd274f1d673adf1
易长松 <xywc_s@foxmail.com>
2021-07-27 15:05:35 +0800

    fix

--- server/src/setting/setting.service.ts
+++ server/src/setting/setting.service.ts
@@ -1,17 +1,17 @@
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Setting } from './../models/setting';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class SettingService {
-  constructor(
-    @InjectRepository(Setting)
-    private setting: Repository<Setting>
-  ){}
+export class SettingService extends TypeOrmCrudService<Setting>{
+  constructor(@InjectRepository(Setting) repository){
+    super(repository)
+  }
 
   async getSetting(){
-    const setting = await this.setting.findOneOrFail()
+    const setting = await this.repo.findOneOrFail()
     if(setting) return setting
     return {
       seo_title: '',
